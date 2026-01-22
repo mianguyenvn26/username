e79c44e2c8e22f30ab8fee819a20fa26fcee51a9
Mia Nguyen <tnguyen28@chapman.edu>
2026-01-21 17:36:53 -0800

    Finalize scripts and remove zip-input

--- username.sh
+++ username.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#Mia Nguyen
+# Mia Nguyen
 # CPSC 298-01
 
 echo -e "Rules for creating a valid username:\nThe only characters that can be used are:\n -lower case letters,\n -digits, and\n -the underscore character"
+echo -e "It must start with a lower case letter\nIt must contain at least three but no more than 12 characters"
 
-
-echo "Enter an username: "
-read -r username
-
-if [[ $username =~ ^[a-z0-9_]*$ ]]; then
+while true; do
+  echo "Enter an username: "
+  read -r username
+  if [[ $username =~ ^[a-z][a-z0-9_]{2,11}$ ]]; then  
     echo "Valid username!"
-else
+    exit
+  else
     echo "Invalid username! Please try again."
 fi
 
+done
 
-
-
- 
